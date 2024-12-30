d0c06421eb0d9af84b33cc67d2be2877c5a0c321
kimth <kimth@stanford.edu>
2024-12-30 11:48:29 -0800

    Not used

--- generate_QP.m
+++
@@ -1,8 +0,0 @@
-function t_qp_threshold = generate_QP()
-
-mu = 2.5; % s
-t_qp_threshold = exprnd(mu);
-
-% Clamp the ITI to [0.5 10]
-t_qp_threshold = min([t_qp_threshold, 5]);
-t_qp_threshold = max([1 t_qp_threshold]); 
